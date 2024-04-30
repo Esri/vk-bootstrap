90a4d2fe31f6a644f1dd1631d482f3b479ca0877
David <dordoneztorres@esri.com>
2024-04-30 10:36:43 +0200

    Add build scripts

---
+++ conanfile_rtc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from conans import ConanFile
+
+
+class VkBootstrapConan(ConanFile):
+    name = "vk-bootstrap"
+    version = "1.3.283"
+    url = "https://github.com/Esri/vk-bootstrap/blob/runtimecore/"
+    license = "https://github.com/Esri/vk-bootstrap/blob/runtimecore/LICENSE.md"
+    description = ("A utility library that jump starts initialization of Vulkan")
+    
+    # RTC specific triple
+    settings = "platform_architecture_target"
+    
+    def package(self):
+        base = self.source_folder + "/"
+        relative = "3rdparty/vk-bootstrap/"
+
+        # headers
+        self.copy("*.h*", src=base, dst=relative)
+
+        # libraries
+        output = "output/" + str(self.settings.platform_architecture_target) + "/staticlib"
+        self.copy("*" + self.name + "*", src=base + "../../" + output, dst=output)
+        
